3673b287b102258681da24596e40b05607d17b29
maz1chek <bedboy0211209@gmail.com>
2024-12-10 18:56:20 +0200

    Add project files.

---
+++ Array/Array.cpp
@@ -0,0 +1,6 @@
+#include "Array.h"
+#include <iostream>
+
+int main() {
+	srand(time(0));
+}
---
+++ Array/Array.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+
+class Array
+{
+private:
+	int* ar;
+	int size;
+
+public:
+	// ¦ Створення пустого масиву на n елементів.
+	Array(int n) : size(n) {
+		ar = new int[size] {};
+	}
+
+	//■ Створення масиву на n елементів з x значеннями iз вказаного діапазону(min / max).
+	Array(int n, int min, int max) : size(n) {
+		ar = new int[size] {};
+		for (int i = 0; i < size; i++)
+		{
+			ar[i] = min + rand() % max + rand();
+		}
+	}
+	//  ■ Деструктор.
+	~Array() {
+		delete[] ar;
+	}
+	//■ Відображати вміст масиву;
+	void display() {
+		for (int i = 0; i < size; i++)
+		{
+			std::cout << ar[i];
+		}
+		std::cout << std::endl;
+	}
+
+};
+
